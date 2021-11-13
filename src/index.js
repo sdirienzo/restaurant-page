e6fdfdfa89a7975c9abaaf0cd402b5678a6c2928
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2021-11-13 11:33:26 -0500

    Introduce pageLoad module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import { generateHomePage } from './homepage.js';
-
-const pageLoad = () => {
-    const container = document.getElementById('content');
-
-    generateHomePage(container);
-
-}
+import { pageLoad } from './page-load.js';
 
 pageLoad();
 
